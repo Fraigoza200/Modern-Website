acadd751e98d1c79e901b390cd9709d93ec27d65
Francisco Raigoza <raigoza.frankie@gmail.com>
2022-10-20 15:57:21 -0700

    Get Started and Header Built

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from '../style';
 import { discount, robot } from '../assets';
+import GetStarted from './GetStarted'
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -8,10 +9,21 @@ const Hero = () => (
     >
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className= {`${styles.paragraph}`}>
-          <span className="text-white">20%</span> Discount For {" "}
+        <p className={`${styles.paragraph}`}>
+          <span className="text-white">20%</span> Discount For{' '}
           <span className="text-white">1 Month</span> Account
         </p>
+      </div>
+
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+          The Next <br className="sm:block hidden" />{' '}
+          <span className="text-gradient">Generation</span> <br/>
+          Payment Method.
+        </h1>
+        <div className='ss:flex hidden md:mr-4 mr-0'>
+            <GetStarted />
+        </div>
       </div>
     </div>
   </section>
