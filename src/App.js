034db438d89885715d85be409ef4f1a9eec9e28d
Francisco Raigoza <raigoza.frankie@gmail.com>
2022-10-20 14:26:47 -0700

    Initial Set Up, Assets, NavBar Built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import './App.css';
+import React from 'react';
+import styles from './style';
+import {
+  NavBar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />{' '}
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{' '}
+          <Clients /> <CTA /> <Footer />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
